46331cfaa4f6de98c090fcfab38d3bfbba6745cb
Austin Rogers <contactALRogers@gmail.com>
2017-05-08 10:08:51 -0700

    Initial Commit

---
+++ Cross-platform-todo-list/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Cross-platform-todo-list
+//
+//  Created by Kent Rogers on 5/8/17.
+//  Copyright © 2017 Austin Rogers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Cross-platform-todo-list/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Cross-platform-todo-list
+//
+//  Created by Kent Rogers on 5/8/17.
+//  Copyright © 2017 Austin Rogers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
